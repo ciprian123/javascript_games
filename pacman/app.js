b5cf7e55dee8183ece6a2d5c49aa6398fe908b5d
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2020-09-19 23:25:10 +0300

    Implemented player basic movement

--- pacman/app.js
+++ pacman/app.js
@@ -3,6 +3,8 @@ import * as Config from "./config.js"
 const userScore = document.querySelector("#score");
 const gameMap = document.querySelector("#map");
 
+let tmpScore = 0;
+
 const mapWidth = 17;
 const mapHeight = 17;
 
@@ -43,43 +45,113 @@ function renderMap() {
     }
 }
 
+function updateUserScore() {
+    if (Config.tiles[playerIndex].classList.contains('pointTile')) {
+        Config.tiles[playerIndex].classList.remove('pointTile');
+        tmpScore++;
+    }
+    userScore.innerHTML = tmpScore;
+}
+
 function movePlayerUp() {
     if (playerIndex - mapHeight >= 0 && !Config.tiles[playerIndex].classList.contains('wallTile')) {
         Config.tiles[playerIndex].classList.remove('playerTile');
         Config.tiles[playerIndex].classList.add('roadTile');
 
         playerIndex -= mapHeight;
-        Config.tiles[playerIndex].classList.remove('pointTile');
+        updateUserScore();
         Config.tiles[playerIndex].classList.add('playerTile');
     }
     else if (playerIndex == 8) {
         // move from top to bottom
         Config.tiles[playerIndex].classList.remove('playerTile');
         playerIndex = 263;
+        updateUserScore();
+        Config.tiles[playerIndex].classList.add('playerTile');
+        Config.tiles[playerIndex].classList.remove('pointTile');
+    }
+}
+
+function movePlayerDown() {
+    if (playerIndex + mapHeight < Config.tiles.length && !Config.tiles[playerIndex].classList.contains('wallTile')) {
+        
+        Config.tiles[playerIndex].classList.remove('playerTile');
+        Config.tiles[playerIndex].classList.add('roadTile');
+
+        playerIndex += mapHeight;
+
+        updateUserScore();
+        Config.tiles[playerIndex].classList.add('playerTile');
+    }
+    else if (playerIndex == 263) {
+        // move from top to bottom
+        Config.tiles[playerIndex].classList.remove('playerTile');
+        playerIndex = 8;
+        updateUserScore();
+        Config.tiles[playerIndex].classList.add('playerTile');
+        Config.tiles[playerIndex].classList.remove('pointTile');
+    }
+}
+
+function movePlayerLeft() {
+    if (playerIndex == 136) {
+        Config.tiles[playerIndex].classList.remove('playerTile');
+        playerIndex = 152;
+        updateUserScore();
+        Config.tiles[playerIndex].classList.add('playerTile');
+        Config.tiles[playerIndex].classList.remove('pointTile');
+    }
+    else if (!Config.tiles[playerIndex].classList.contains('wallTile')) {
+        Config.tiles[playerIndex].classList.remove('playerTile');
+        playerIndex--;
+        updateUserScore();
+        Config.tiles[playerIndex].classList.add('playerTile');
+    }
+    else {
+        Config.tiles[playerIndex].classList.remove('playerTile');
+        playerIndex += mapWidth;
+        Config.tiles[playerIndex].classList.add('playerTile');
+        Config.tiles[playerIndex].classList.remove('pointTile');
+    }
+}
+
+function movePlayerRight() {
+    if (playerIndex == 152) {
+        Config.tiles[playerIndex].classList.remove('playerTile');
+        playerIndex = 136;
+        updateUserScore();
+        Config.tiles[playerIndex].classList.add('playerTile');
+        Config.tiles[playerIndex].classList.remove('pointTile');
+    }
+    else if (!Config.tiles[playerIndex].classList.contains('wallTile')) {
+        Config.tiles[playerIndex].classList.remove('playerTile');
+        playerIndex++;
+        updateUserScore();
+        Config.tiles[playerIndex].classList.add('playerTile');
+    }
+    else {
+        Config.tiles[playerIndex].classList.remove('playerTile');
+        playerIndex -= mapWidth;
+        Config.tiles[playerIndex].classList.add('playerTile');
+        Config.tiles[playerIndex].classList.remove('pointTile');
     }
 }
 
 function movePlayer(event) {
-    // player moves up
     if (event.keyCode == 38) {
-        if (playerIndex - mapHeight >= 0 && !Config.tiles[playerIndex].classList.contains('wallTile')) {
-            Config.tiles[playerIndex].classList.remove('playerTile');
-            Config.tiles[playerIndex].classList.add('roadTile');
-
-            playerIndex -= mapHeight;
-            Config.tiles[playerIndex].classList.remove('pointTile');
-            Config.tiles[playerIndex].classList.add('playerTile');
-        }
-        else if (playerIndex == 8) {
-            // move from top to bottom
-            Config.tiles[playerIndex].classList.remove('playerTile');
-            playerIndex = 263;
-        }
-        
+        movePlayerUp();
+    }
+    else if (event.keyCode == 40) {
+        movePlayerDown();
+    }
+    else if (event.keyCode == 37) {
+        movePlayerLeft();
+    }
+    else if (event.keyCode == 39) {
+        movePlayerRight();
     }
 }
 
 renderMap(); 
 
 document.addEventListener('keyup', movePlayer);
-document.addEventListener('keydown', movePlayer);
